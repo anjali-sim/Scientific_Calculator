d7c0b458a39a5bec4b66dc8029ee49cc549aadb8
anjali-sim <anjalighetia123@gmail.com>
2023-03-01 10:06:30 +0530

    Done with Trigonometric functions

--- src/script/script.js
+++ src/script/script.js
@@ -1,2 +1,24 @@
+function display(value) {
+    input.value += value;
+       
+}
 
-
+// Trigonometry Functions
+function sin() {
+input.value = (Math.sin(Math.PI / 180 * input.value));
+}
+function cos() {
+input.value = (Math.cos(Math.PI / 180 * input.value));
+}
+function tan() {
+input.value = (Math.tan(Math.PI / 180 * input.value));
+}
+function cot() {
+input.value = (1 / Math.tan(Math.PI / 180 * input.value));
+}
+function sec() {
+input.value = (1 / Math.cos(Math.PI / 180 * input.value));
+}
+function cosec() {
+input.value = (1 / Math.sin(Math.PI / 180 * input.value));
+}
